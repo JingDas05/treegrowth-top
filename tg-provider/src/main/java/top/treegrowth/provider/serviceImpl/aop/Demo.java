25726ea5d7c0664910da2663883d7d3f340827a1
wusi05 <wusi05@126.com>
2017-03-18 09:01:03 +0800

    添加hadoop依赖

--- tg-provider/src/main/java/top/treegrowth/provider/serviceImpl/aop/Demo.java
+++ tg-provider/src/main/java/top/treegrowth/provider/serviceImpl/aop/Demo.java
@@ -1,9 +1,15 @@
 package top.treegrowth.provider.serviceImpl.aop;
 
+import java.lang.annotation.*;
+
 /**
  * @author wusi
  * @version 2017/3/18 8:24.
  */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@Inherited
 public @interface Demo {
 
     String name();
---
+++ tg-provider/src/main/java/top/treegrowth/provider/serviceImpl/hadoop/HelloWorld.java
@@ -0,0 +1,10 @@
+package top.treegrowth.provider.serviceImpl.hadoop;
+
+/**
+ * @author wusi
+ * @version 2017/3/18 9:00.
+ */
+public class HelloWorld {
+
+
+}

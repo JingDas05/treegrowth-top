d69ae5b34221a9cc7bf806b1c6471b0c0cc07cc9
wusi05 <wusi05@126.com>
2017-03-22 07:34:54 +0800

    修改 spring batch 配置

--- tg-provider/src/main/java/top/treegrowth/ProviderApplication.java
+++ tg-provider/src/main/java/top/treegrowth/ProviderApplication.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ImportResource;
 
 /**
  * @author wusi
@@ -11,7 +12,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
-@MapperScan(basePackages = {"top.treegrowth.provider.dao.top.treegrowth.mapper"})
+@ImportResource
+//@MapperScan(basePackages = {"top.treegrowth.provider.dao.top.treegrowth.mapper"})
 public class ProviderApplication {
 
 

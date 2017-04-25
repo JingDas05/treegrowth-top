cdabb734f0b1e55625285fb24af24f911895e920
wusi05 <wusi05@126.com>
2017-04-25 08:06:02 +0800

    修改pom文件，准备打包

--- tg-consumer/src/test/java/top/treegrowth/consumer/TestConfig.java
+++ tg-consumer/src/test/java/top/treegrowth/consumer/TestConfig.java
@@ -6,14 +6,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
-@Configuration
-@ComponentScan({ "top.treegrowth.consumer.web" })
+//@Configuration
+//@ComponentScan({ "top.treegrowth.consumer.web" })
 public class TestConfig {
 
-    @Bean
-    public MultipartResolver multipartResolver() {
-        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
-        return multipartResolver;
-    }
+//    @Bean
+//    public MultipartResolver multipartResolver() {
+//        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+//        return multipartResolver;
+//    }
 
 }

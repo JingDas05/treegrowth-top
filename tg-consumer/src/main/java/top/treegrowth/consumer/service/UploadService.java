347b526cdd9de17a49fbff83d7089224cc4c864d
wusi05 <wusi05@126.com>
2017-06-22 22:58:10 +0800

    添加feignclient 支持multipart encoder

--- tg-consumer/src/main/java/top/treegrowth/consumer/service/UploadService.java
+++ tg-consumer/src/main/java/top/treegrowth/consumer/service/UploadService.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+import top.treegrowth.consumer.config.MultipartSupportConfig;
 
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
@@ -12,7 +13,7 @@ import static org.springframework.web.bind.annotation.RequestMethod.POST;
  * @author wusi
  * @version 2017/6/21 22:03.
  */
-@FeignClient("provider")
+@FeignClient(name = "provider", configuration = MultipartSupportConfig.class)
 @RequestMapping(value = "api/service")
 public interface UploadService {
 
